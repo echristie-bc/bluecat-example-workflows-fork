96598e731b33a6b8e521871cb328641d54d36a86
abartella-bc <abartella@bluecatnetworks.com>
2024-02-12 14:46:51 -0500

    ENG-54380: Invalid text input field validation for configuration name in Configuration Details workflow

--- projects/configuration_details/configuration_details_ui/src/pages/configurationDetails/App.js
+++ projects/configuration_details/configuration_details_ui/src/pages/configurationDetails/App.js
@@ -52,12 +52,7 @@ import {
     TableToolbar,
     TableToolbarDefault,
 } from '@bluecateng/pelagos';
-import {
-    Form,
-    validateAnd,
-    validateMatches,
-    validateNotEmpty,
-} from '@bluecateng/auto-forms';
+import { Form, validateNotEmpty } from '@bluecateng/auto-forms';
 import { FormSubmit, FormTextInput } from '@bluecateng/pelagos-forms';
 import './App.less';
 
@@ -109,13 +104,7 @@ const Content = () => {
 
     // Rules for the add Configuration panel
     const configurationRules = {
-        name: validateAnd([
-            validateMatches(
-                /^[a-zA-Z0-9][a-zA-Z0-9_-]*$/,
-                'Invalid configuration name.',
-            ),
-            validateNotEmpty('The configuration name is required.'),
-        ]),
+        name: validateNotEmpty('The configuration name is required.'),
     };
 
     const closeAllPanelsAndSetValuesToDefault = () => {
